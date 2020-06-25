cc85566d7c6254fb601a1fea9d7122a07a1aacee
Rishi Vijay <rishi23vj@gmail.com>
2020-06-25 08:42:59 +0530

    yuhu

--- myapp/index.js
+++ myapp/index.js
@@ -1,7 +1,14 @@
 var express = require('express');
-var studentModel = require('../modules/student');
-var router = express.Router();
+var studentModel = require('./modules/student');
+var router = express();
+var path = require('path');
 var studentDetails = studentModel.find({});
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
+router.use(express.static(path.join(__dirname, 'public')));
+router.set('views', path.join(__dirname, 'views'));
+router.set('view engine', 'ejs');
+
 
 
 
@@ -67,13 +74,67 @@ router.post("/search/", function (req, res, next) {
   var studentFilter = studentModel.find(filterParameter);
 
 
-studentFilter.exec(function (err, data) {
-  if (err)
-    throw err;
-  res.render('index', { title: 'Student Records', records: data });
-})
+  studentFilter.exec(function (err, data) {
+    if (err)
+      throw err;
+    res.render('index', { title: 'Student Records', records: data });
+  })
 
 });
 
 
-module.exports = router;
+router.get('/delete/:id', function (req, res, next) {
+
+  var id = req.params.id;
+  var del = studentModel.findByIdAndDelete(id);
+  del.exec(function (err) {
+    if (err)
+      throw err;
+    res.redirect("/");
+  })
+
+});
+
+
+router.get('/edit/:id', function (req, res, next) {
+
+  var id = req.params.id;
+  var edit = studentModel.findById(id);
+  edit.exec(function (err, data) {
+    if (err)
+      throw err;
+    res.render('edit', { title: 'Edit Student Record', records: data });
+  })
+
+});
+
+router.post('/update/', function (req, res, next) {
+
+  var id = req.body.id;
+  var update = studentModel.findByIdAndUpdate(id,{
+    Name: req.body.uname,
+    Rollno: req.body.roll,
+    email: req.body.email,
+    DOB: req.body.dob,
+    Address: req.body.address
+  });
+
+
+  update.exec(function (err, data) {
+    if (err)
+      throw err;
+    res.redirect("/")
+  })
+
+});
+
+
+
+router.listen(process.env.PORT || 3000, function () {
+  console.log('Your node js server is running');
+});
+
+
+
+
+
